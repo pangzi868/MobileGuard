67455c6d8cb1562304420fcf53fea562c2391264
563913106@qq.com <31606321+wangxiaoyao2639@users.noreply.github.com>
2017-10-15 22:16:56 +0800

    还有一个登录问题

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/entity/VersionEntity.java
@@ -0,0 +1,11 @@
+package cn.edu.gdmec.android.mobileguard.m1home.entity;
+
+/**
+ * Created by Shinelon on 2017/9/27.
+ */
+
+public class VersionEntity {
+    public String versioncode;
+    public String description;
+    public String apkurl;
+}
