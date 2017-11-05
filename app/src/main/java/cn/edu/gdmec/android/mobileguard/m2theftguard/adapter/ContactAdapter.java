c94a77084813eeea4d548f0c638c7dba7f479ef1
563913106@qq.com <31606321+wangxiaoyao2639@users.noreply.github.com>
2017-11-05 20:43:23 +0800

    11.05 求别撞车

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/adapter/ContactAdapter.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/adapter/ContactAdapter.java
@@ -1,6 +1,7 @@
 package cn.edu.gdmec.android.mobileguard.m2theftguard.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -12,46 +13,64 @@ import cn.edu.gdmec.android.mobileguard.R;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.entity.ContactInfo;
 
 /**
- * Created by 头兄。 on 2017/10/22.
+ * Created by Administrator on 2017/10/21.
  */
 
-public class ContactAdapter extends BaseAdapter{
+public class ContactAdapter extends BaseAdapter {
     private List<ContactInfo> contactInfos;
+    static int count = 1;
     private Context context;
+    //    private TextView mNameTV;
+//    private TextView mPhoneTV;
     public ContactAdapter(List<ContactInfo> contactInfos,Context context){
         super();
         this.contactInfos = contactInfos;
         this.context = context;
-    }
 
-    public int getCount(){
+    }
+    @Override
+    public int getCount() {
+        Log.d("Tag", "getItem: ----------------系统调用getitem 集合长度为："+contactInfos.size());
         return contactInfos.size();
     }
 
-    public Object getItem(int i){
-        return contactInfos.get(i);
+    @Override
+    public Object getItem(int position) {
+
+        return contactInfos.get(position);
     }
 
-    public long getItemId(int i){
-        return i;
+    @Override
+    public long getItemId(int position) {
+        return position;
     }
 
-    public View getView(int i,View view,ViewGroup viewGroup){
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        Log.d("Tag", "getView:  -----------------系统调用 "+(count++)+"次  然后集合长度为"+contactInfos.size());
         ViewHolder holder = null;
         if (view == null){
             view = View.inflate(context, R.layout.item_list_contact_select,null);
             holder = new ViewHolder();
             holder.mNameTV = (TextView) view.findViewById(R.id.tv_name);
             holder.mPhoneTV = (TextView) view.findViewById(R.id.tv_phone);
+//            mNameTV = (TextView) view.findViewById(R.id.tv_name1);
+//            mPhoneTV = (TextView) view.findViewById(R.id.tv_phone);
             view.setTag(holder);
         }else{
-            holder = (ViewHolder) view.getTag();
+            holder= (ViewHolder) view.getTag();
+
         }
-        holder.mNameTV.setText(contactInfos.get(i).name);
-        holder.mPhoneTV.setText(contactInfos.get(i).phone);
+        Log.d("Tag",  holder.mNameTV == null?"true":"false");
+        Log.d("Tag",  holder.mPhoneTV == null?"true":"false");
+//        mNameTV.setText(contactInfos.get(position).name);
+//        mPhoneTV.setText(contactInfos.get(position).phone);
+        holder.mNameTV.setText(contactInfos.get(position).name);
+        holder.mPhoneTV.setText(contactInfos.get(position).phone);
+        Log.d("Tag", "getView:  holder.mNameTV.setText(contactInfos.get(position).name); end ");
         return view;
     }
-    static class ViewHolder{
+    static class ViewHolder {
         TextView mNameTV;
         TextView mPhoneTV;
     }
