2cd74979b127b88b6f14b7922dff49aca631fd80
yaoa <563913106@qq.com>
2017-12-12 11:57:45 +0800

    12.11  AppLockActivity.java66行

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/AppLockActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/AppLockActivity.java
@@ -1,8 +1,70 @@
 package cn.edu.gdmec.android.mobileguard.m9advancedtools;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.edu.gdmec.android.mobileguard.R;
+
 /**
  * Created by student on 17/12/11.
  */
 
-public class AppLockActivity {
+public class AppLockActivity extends AppCompatActivity implements View.OnClickListener{
+    private ViewPager mAppViewPager;
+    List<Fragment> mFragments = new ArrayList<Fragment>();
+    private TextView mLockTV;
+    private TextView mUnLockTV;
+    private View slideLockView;
+    private View slideUnLockView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_lock);
+        initView();
+        initListener();
+    }
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.imgv_leftbtn:
+                finish();
+                break;
+            case R.id.tv_lock:
+                mAppViewPager.setCurrentItem(1);
+                break;
+            case R.id.tv_unlock:
+                mAppViewPager.setCurrentItem(0);
+                break;
+        }
+    }
+
+    private void initListener(){
+        mAppViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener(){
+            @Override
+            public void onPageSelected(int arg0){
+                if(arg0 == 0){
+                    slideUnLockView.setBackgroundResource(R.drawable.slide_view);
+                    slideLockView.setBackgroundColor(getResources().getColor(R.color.transparent));
+                    //未加锁
+                    mLockTV.setTextColor(getResources().getColor(R.color.black));
+                    mUnLockTV.setTextColor(getResources().getColor(R.color.bright_red));
+                }else{
+                    slideLockView.setBackgroundResource(R.drawable.slide_view);
+                    slideUnLockView.setBackgroundColor(getResources().getColor(R.color.transparent));
+                    //已加锁
+                    mLockTV.setTextColor(getResources().getColor(R.color.bright_red));
+                    mUnLockTV.setTextColor(getResources().getColor(R.color.black));
+                }
+            }
+        });
+    }
 }
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/EnterPswActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/EnterPswActivity.java
@@ -7,13 +7,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import cn.edu.gdmec.android.mobileguard.App;
 import cn.edu.gdmec.android.mobileguard.R;
+import cn.edu.gdmec.android.mobileguard.m2theftguard.utils.MD5Utils;
 
 /**
  * Created by student on 17/12/11.
@@ -61,7 +65,7 @@ public class EnterPswActivity extends AppCompatActivity implements View.OnClickL
     }
 
     @Override
-    public void onCreate(View view){
+    public void onClick(View view){
         switch (view.getId()){
             case R.id.imgv_go_enterpsw:
                 //比较密码
@@ -70,8 +74,27 @@ public class EnterPswActivity extends AppCompatActivity implements View.OnClickL
                     startAnim();
                     Toast.makeText(this,"请输入密码！",Toast.LENGTH_SHORT).show();
                     return;
+                }else{
+                    if(!TextUtils.isEmpty(password)){
+                        if(MD5Utils.encode(inputpsw).equals(password)){
+                            //发送自定义的广播消息
+                            Intent intent = new Intent();
+                            intent.setAction(App.APPLOCK_ACTION);
+                            intent.putExtra("packagename",packagename);
+                            sendBroadcast(intent);
+                        }else{
+                            startAnim();
+                            Toast.makeText(this,"密码不正确！",Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }
                 }
+                break;
         }
+    }
+    private void startAnim(){
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.shake);
+        mEnterPswLL.startAnimation(animation);
     }
 
 }
