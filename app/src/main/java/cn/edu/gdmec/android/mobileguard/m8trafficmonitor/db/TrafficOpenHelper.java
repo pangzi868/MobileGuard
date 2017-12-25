8993174c09f2e27c662c0340813022613fb8154b
563913106@qq.com <31606321+wangxiaoyao2639@users.noreply.github.com>
2017-12-25 16:55:29 +0800

    12.25

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/db/TrafficOpenHelper.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/db/TrafficOpenHelper.java
@@ -8,27 +8,24 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by 头兄。 on 2017/12/3.
  */
 
-public class TrafficOpenHelper extends SQLiteOpenHelper {
-    private static final String DB_NAME = "traffic.db";
-    private static final String TABLE_NAME = "traffic";
+public class TrafficOpenHelper extends SQLiteOpenHelper{
+    private static final String DB_NAME="traffic.db";
+    private static final String TABLE_NAME="traffic";
     /*流量*/
-    private final static String GPRS = "gprs";
-    private final static String TIME = "date";
-
+    private final static String GPRS="gprs";
+    private final static String TIME="date";
     public TrafficOpenHelper(Context context){
         super(context,DB_NAME,null,1);
-
     }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table "+TABLE_NAME+
-                "(id integer primary key autoincrement,"
-                +GPRS+" varchar(255),"+
-                TIME+" datetime)");
+        db.execSQL("create table "+ TABLE_NAME + " (id integer primary key autoincrement," + GPRS + " varchar(255)," + TIME + " datetime)");
+
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
     }
 }
